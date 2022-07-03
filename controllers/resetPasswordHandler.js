860cd6304662a7fb20b54334d62f75754997af16
Khandakar <xenonknight54@gmail.com>
2022-07-03 17:19:48 +0600

    Updated reset password route

--- controllers/resetPasswordHandler.js
+++ controllers/resetPasswordHandler.js
@@ -1,30 +1,59 @@
+const { hash } = require("bcryptjs");
 const express = require("express");
+const { validationResult } = require("express-validator");
 const { verify } = require("jsonwebtoken");
-
-  /**
-   * @param {express.Request} req
-   * @param {express.Response} res
-   * @param {express.NextFunction} next
-   */
-  async function resetPassword(req, res, next) {
-    if (req.method === "GET") {
-      const {token} = req.params;
-      // const payload = verify(token, process.env.EMAIL_SECRET);
-     
-      res.status(200).send(passwordResetInput(token));
-    } else if (req.method === "POST") {
-        return 
-    }
-  };
+const User = require("../models/user");
 
 /**
- * 
- * @param {string} token 
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+async function resetPassword(req, res, next) {
+  try {
+    const { token } = req.params;
+    verify(token, process.env.EMAIL_SECRET, async function (err, decoded) {
+      //Check if token is valid
+      if (err) {
+        res.status(400).json({
+          message: "Invalid token!",
+        });
+
+        //Request body validation result. Throw error for invalid values.
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+          return res.status(400).json({ errors: errors.array() });
+
+        //For get request send a password changing form
+        if (req.method === "GET") {
+          res.status(200).send(passwordResetInput(token));
+        }
+        //For post request reset given password
+      } else if (req.method === "POST") {
+        const payload = decoded;
+        const { password } = req.body;
+        const hashPassword = await hash(password, 10);
+        if (payload && payload._id) {
+          await User.findByIdAndUpdate(_id, {
+            password: hashPassword,
+          });
+          res.status(201).json({ message: "Password changed successfully." });
+        }
+      }
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
+
+/**
+ *
+ * @param {string} token
  * @returns
  */
 function passwordResetInput(token) {
-    const title = "Reset your password"
-    const url = "http://localhost:8000/api/resetpassword/"+token;
+  const title = "Reset your password";
+  const url = "http://localhost:8000/api/resetpassword/" + token;
   return `
   <!DOCTYPE html>
 <html>
@@ -39,7 +68,7 @@ function passwordResetInput(token) {
         <h2> Reset password </h2>
         <form>
             <label for="password"> Enter new password:</label>
-            <input type="password" name="password" aria-label="password">
+            <input type="password" name="password" aria-label="password" required>
             <input type="submit" name="change password" aria-label="change password" value="Change password">
         </form>
 
@@ -59,6 +88,5 @@ function passwordResetInput(token) {
 
     `;
 }
-
 
 module.exports = resetPassword;
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", userRoutes);
 app.use("/api/verify", verifyRoutes);
