f938155dcb6025eaaa0238b8666c069c28aecfb2
Khandakar <xenonknight54@gmail.com>
2022-07-04 20:55:11 +0600

    Added rate limiter flexible

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const helmet = require("helmet");
 const userRoutes = require("./routes/user");
 const verifyRoutes = require("./routes/verify");
 const forgotPasswordRoutes = require("./routes/forgotpassword");
 const resetPasswordRoutes = require("./routes/resetpassword");
 const { connect } = require("mongoose");
+const rateLimiterMiddleware = require("./middlewares/rateLimiterFlexible");
 
 dotenv.config();
 
@@ -15,6 +17,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", userRoutes);
@@ -22,13 +25,16 @@ app.use("/api/verify", verifyRoutes);
 app.use("/api/forgotpassword", forgotPasswordRoutes);
 app.use("/api/resetpassword", resetPasswordRoutes);
 
+
 connect(process.env.MONGODB_URL)
   .then(() => {
     console.log("connected to database!");
   })
-  .catch(() => {
-    console.log("connection failed! ");
+  .catch((error) => {
+    console.log("connection failed! ", error);
   });
+
+app.use(rateLimiterMiddleware);
 
 app.listen(PORT, () => {
   console.log("server started running at port", PORT);
---
+++ middlewares/rateLimiterFlexible.js
@@ -0,0 +1,28 @@
+const { RateLimiterMongo } = require("rate-limiter-flexible");
+const {connection} = require("mongoose");
+
+const rateLimiterMongo = new RateLimiterMongo({
+  storeClient: connection,
+  points: 10, // Number of points
+  duration: 1, // Per second(s)
+});
+
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+
+const rateLimiterMiddleware = (req, res, next) => {
+  rateLimiterMongo
+    .consume(req.ip)
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      res.status(429).send("Too Many Requests");
+    });
+};
+
+module.exports = rateLimiterMiddleware;
